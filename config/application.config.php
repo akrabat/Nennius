1932f6c7c6a8e87469e614f44fca0f6531937f38
Rob Allen <rob@akrabat.com>
2011-12-28 18:36:00 +0000

    add moduledev

--- config/application.config.php
+++ config/application.config.php
@@ -10,6 +10,7 @@ return array(
         'config_cache_enabled' => false,
         'cache_dir'            => 'data/cache',
         'module_paths' => array(
+            './moduledev',
             './module',
             './vendor',
         ),
